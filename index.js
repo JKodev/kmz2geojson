fd59a8f9f20992e9e6a54647fcb7bef74c9ded58
Kodev <jonadotnet@gmail.com>
2017-11-14 17:52:00 -0500

    Removed innecesary console log.

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
-const fs = require('fs');
-const kmz = require('./kmz');
-
-console.log(process.argv);
+const fs = require("fs");
+const kmz = require("./kmz");
 
 const argv = process.argv;
 
 if (argv.length < 4) {
-	exit();
+  exit();
 }
 
 const directory = argv[2];
 const target = argv[3];
 
 fs.readdir(directory, (err, files) => {
-	console.log(files);
-	files.forEach(file => {
-		if (file.indexOf('kmz') !== -1) {
-			const path = `${directory}${file}`;
-			const targetPath = `${target}${file}`;
+  files.forEach(file => {
+    if (file.indexOf("kmz") !== -1) {
+      const path = `${directory}${file}`;
+      const targetPath = `${target}${file}`;
 
-			kmz.toGeoJson(path, (error, geoJson) => {
-				const newPath = targetPath.split('.');
-				newPath.pop();
-				newPath.push('geojson');
+      kmz.toGeoJson(path, (error, geoJson) => {
+        const newPath = targetPath.split(".");
+        newPath.pop();
+        newPath.push("geojson");
 
-				fs.writeFile(newPath.join('.'), JSON.stringify(geoJson), (err) => {
-					console.log(err);
-				});
-			});
-		}
-	});
+        fs.writeFile(newPath.join("."), JSON.stringify(geoJson), err => {
+          console.log(err);
+        });
+      });
+    }
+  });
 });
